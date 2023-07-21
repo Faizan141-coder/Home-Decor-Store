caa32d3fd891a26cb10d9783b4ba9ef9a8673cf8
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-07-21 11:18:54 +0500

    solved featured page billboard bug

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("58f58cb6-ad8a-4de2-bc4d-ceb52d3fb5ea");
 
   return (
     <Container>
